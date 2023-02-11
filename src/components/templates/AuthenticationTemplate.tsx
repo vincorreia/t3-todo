a2d3609d5738c3eecf25ea0a061d11f508ebf84b
vincorreia <vcorreiaes@gmail.com>
2023-02-11 17:05:45 +0100

    FIX ( Sign in page ): Sign in button style and literal

---
+++ src/components/templates/AuthenticationTemplate.tsx
@@ -0,0 +1,50 @@
+type AuthShowcaseProps = {
+  children: React.ReactNode;
+};
+import { useSession, signIn } from "next-auth/react";
+import { type NextPage } from "next";
+import Head from "next/head";
+import { Loading } from "../atoms/Loading";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+export const AuthenticationTemplate: NextPage<AuthShowcaseProps> = ({
+  children,
+}) => {
+  const { status } = useSession();
+
+  if (status === "authenticated") {
+    return <>{children}</>;
+  }
+
+  if (status === "loading") {
+    return (
+      <>
+        <Head>
+          <title>Loading...</title>
+        </Head>
+        <Loading />
+      </>
+    );
+  }
+
+  return (
+    <>
+      <Head>
+        <title>Sign in</title>
+      </Head>
+      <div className="flex h-full w-full flex-col justify-center gap-4">
+        <h1 className="text-5xl text-white">Welcome!</h1>
+        <p className="text-xl text-white">
+          Please sign in to continue using the app.
+        </p>
+        <button
+          className="flex items-center gap-x-2 self-end rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
+          onClick={() => void signIn("google")}
+        >
+          <FontAwesomeIcon size="lg" icon={faGoogle} />
+          {"Sign in with Google"}
+        </button>
+      </div>
+    </>
+  );
+};
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,10 +10,7 @@ config.autoAddCss = false;
 import { Toast } from "../components/atoms/Toast";
 import { SessionProvider } from "next-auth/react";
 import type { Session } from "next-auth/core/types";
-import { useSession, signIn } from "next-auth/react";
-import { type NextPage } from "next";
-import Head from "next/head";
-import { Loading } from "../components/atoms/Loading";
+import { AuthenticationTemplate } from "../components/templates/AuthenticationTemplate";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -21,65 +18,15 @@ const MyApp: AppType<{ session: Session | null }> = ({
 }) => {
   return (
     <SessionProvider session={session}>
-      <Layout>
-        <AuthShowcase>
-          <Component {...pageProps} />
-        </AuthShowcase>
-      </Layout>
+      <main className="h-screen overflow-auto bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+        <div className="container mx-auto flex h-full max-w-3xl flex-grow flex-col items-center gap-y-8 py-4">
+          <AuthenticationTemplate>
+            <Component {...pageProps} />
+          </AuthenticationTemplate>
+        </div>
+      </main>
       <Toast />
     </SessionProvider>
-  );
-};
-const Layout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <main className="h-screen overflow-auto bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <div className="container mx-auto flex h-full max-w-3xl flex-grow flex-col items-center gap-y-8 py-4">
-        {children}
-      </div>
-    </main>
-  );
-};
-
-type AuthShowcaseProps = {
-  children: React.ReactNode;
-};
-const AuthShowcase: NextPage<AuthShowcaseProps> = ({ children }) => {
-  const { status } = useSession();
-
-  if (status === "authenticated") {
-    return <>{children}</>;
-  }
-
-  if (status === "loading") {
-    return (
-      <>
-        <Head>
-          <title>Loading...</title>
-        </Head>
-        <Loading />
-      </>
-    );
-  }
-
-  return (
-    <>
-      <Head>
-        <title>Sign in</title>
-      </Head>
-      <div className="flex h-full w-full flex-col justify-center gap-4">
-        <h1 className="text-5xl text-white">Welcome!</h1>
-        <p className="text-xl text-white">
-          Please sign in to continue using the app.
-        </p>
-        <button
-          className="self-end rounded-full bg-white/10 px-10 py-3 font-semibold text-white no-underline transition hover:bg-white/20"
-          onClick={() => void signIn("google")}
-        >
-          {"Sign in"}
-        </button>
-        
-      </div>
-    </>
   );
 };
 
