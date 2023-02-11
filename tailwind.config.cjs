8e7222c4476eed34c675af10436a0485c7496d21
vincorreia <vcorreiaes@gmail.com>
2023-02-11 10:37:58 +0100

    FEATURE: Loading screen

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        jumping: {
+          "0%, 50%": { transform: "translateY(0)" },
+          "25%": { transform: "translateY(-150%)" },
+        },
+      },
+      animation: {
+        jumping: "jumping 2s ease-in 0.1s infinite",
+        jumping1: "jumping 2s ease-in 0.2s infinite",
+        jumping2: "jumping 2s ease-in 0.3s infinite",
+      },
+    },
   },
   plugins: [],
 };
