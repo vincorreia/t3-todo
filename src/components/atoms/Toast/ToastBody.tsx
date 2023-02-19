10a7ab22212367e0f603a7b88dad9b1a55384818
vincorreia <vcorreiaes@gmail.com>
2023-02-20 00:00:05 +0100

    VISUAL: Toast on mobile

--- src/components/atoms/Toast/ToastBody.tsx
+++ src/components/atoms/Toast/ToastBody.tsx
@@ -36,16 +36,16 @@ export const ToastBody: React.FC<Props> = ({ type, message }) => {
   return (
     <>
       <div
-        className={`flex items-center gap-x-8 px-8 py-4 text-white ${colors[type]}`}
+        className={`flex items-center gap-x-4 lg:gap-x-8 px-8 py-2 lg:py-4 text-white ${colors[type]}`}
       >
         <FontAwesomeIcon
           icon={icon[type]}
           spin={type === "loading"}
           size="3x"
         />
-        <div className="flex flex-col gap-y-4">
-          <p className="text-2xl font-bold capitalize">{type}</p>
-          <p className="text-xl font-semibold">{message}</p>
+        <div className="flex flex-col gap-y-2 lg:gap-y-4">
+          <p className="lg:text-2xl text-xl font-bold capitalize">{type}</p>
+          <p className="lg:text-xl font-semibold">{message}</p>
         </div>
         <div className="flex flex-grow items-end justify-end self-start">
           <ActionButton onClick={handleClose} className="w-4" icon={faX} />
